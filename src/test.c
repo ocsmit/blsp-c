92dcb8d8692ced9710728181dbe5430eaad12023
ocsmit <osmith2@ncsu.edu>
2024-02-28 14:46:38 -0500

    [FEAT] check file ptr in test

--- src/test.c
+++ src/test.c
@@ -1,9 +1,14 @@
-//#define DEBUG
+// #define DEBUG
+#include "blsp.h"
 #include "blsp_math.h"
 #include "common.h"
 #include "timeseries.h"
-#include "blsp.h"
 
+#define check_file(fptr)                                                       \
+  if (!fptr) {                                                                 \
+    printf("Error! Could not open file\n");                                    \
+    exit(-1);                                                                  \
+  }
 
 void fprint_gslmat(const gsl_matrix *m) {
   for (size_t i = 0; i < m->size1; ++i) {
@@ -21,14 +26,12 @@ void print_gslmat(const gsl_matrix *m) {
   }
 }
 
-
 void fprint_gslvec(const gsl_vector *m) {
   for (size_t i = 0; i < m->size; ++i) {
-      fprintf(stderr, "%f ", gsl_vector_get(m, i));
+    fprintf(stderr, "%f ", gsl_vector_get(m, i));
   }
-   fprintf(stderr, "\n");
+  fprintf(stderr, "\n");
 }
-
 
 // MAIN ***********************************************************************
 int main(int argc, char const *argv[]) {
@@ -37,12 +40,10 @@ int main(int argc, char const *argv[]) {
 
   // Parameters taken from simulated data in R
   double theta_mean[7] = {-1.998392,  0.960355, 120.702350, 9.263498,
-                                    288.853856, 9.166685, -6.592421};
+                          288.853856, 9.166685, -6.592421};
 
-  double theta_sd[7] = {0.07057906, 0.05609551, 1.08944966,
-                                  0.88183154, 1.55979462, 1.20727157,
-                                  0.19881890};
-
+  double theta_sd[7] = {0.07057906, 0.05609551, 1.08944966, 0.88183154,
+                        1.55979462, 1.20727157, 0.19881890};
 
   // Copy over theta mean and theta standard deviation to GSL_Vectors
   // Could also just create a GSL array view to the two arrays but would rather
@@ -65,14 +66,17 @@ int main(int argc, char const *argv[]) {
 
   // Read in test data. GLS will throw error if file not found
   FILE *file_doy = fopen("./data/doy_full.dat", "r");
+  check_file(file_doy);
   gsl_vector_fscanf(file_doy, doy_vector);
   fclose(file_doy);
 
   FILE *file_y = fopen("./data/y_full.dat", "r");
+  check_file(file_y);
   gsl_vector_fscanf(file_y, y_vector);
   fclose(file_y);
 
   FILE *file_yr_id = fopen("./data/year_idx.dat", "r");
+  check_file(file_yr_id);
   gsl_vector_fscanf(file_y, yr_id_vector);
   fclose(file_yr_id);
 
@@ -81,7 +85,8 @@ int main(int argc, char const *argv[]) {
   gsl_vector_memcpy(X->data, y_vector);
   gsl_vector_memcpy(X->time, doy_vector);
 
-  /* gsl_vector_view idx_view = gsl_vector_view_array(year_idx_vec, nyrs + 1); */
+  /* gsl_vector_view idx_view = gsl_vector_view_array(year_idx_vec, nyrs + 1);
+   */
   /* gsl_vector_memcpy(X->tidx, &idx_view.vector); */
 
   // Read in test data. GLS will throw error if file not found
@@ -95,9 +100,8 @@ int main(int argc, char const *argv[]) {
   /// TimeSeries_t *Xi = TimeSeries_init(nyrs, nobs, &data_view.vector,
   /// &doy_view.vector, &yr_idx_view.vector);
 
-
-
-  blsp_workspace *w = blsp_workspace_sampler_alloc(num_obs, num_years, 2000, 7000, 7);
+  blsp_workspace *w =
+      blsp_workspace_sampler_alloc(num_obs, num_years, 2000, 7000, 7);
 
   blsp_sampler(X, theta_mean_view, theta_sd_view, w);
 
