fc6ec83aa56a36567e90c1bfa5116fa4ccfa7ca6
ocsmit <osmith2@ncsu.edu>
2024-03-01 11:08:23 -0500

    [REFACTORING] Update R ffi to reflect updated C interface

--- r/src/blsp_r.c
+++ r/src/blsp_r.c
@@ -13,12 +13,6 @@
 #include <gsl/gsl_vector.h>
 #include <R_ext/Print.h>
 
-#define GSL_TO_REALSXP(dst, src, size)                                         \
-  memcpy(REAL(dst), src, sizeof(double) * size);
-
-#define GSLVEC_TO_REALSXP(dst, src) GSL_TO_REALSXP(dst, src->data, src->size)
-#define GSLMAT_TO_REALSXP(dst, src)                                            \
-  GSL_TO_REALSXP(dst, src->data, src->size1 * src->size2)
 
 SEXP run_blsp(SEXP data_vector, SEXP doy_vector, SEXP year_idx_vector,
               SEXP init_theta_mu, SEXP init_theta_sd, SEXP iterations,
@@ -42,35 +36,35 @@ SEXP run_blsp(SEXP data_vector, SEXP doy_vector, SEXP year_idx_vector,
   // we have enough memory for it to fit, but I could instead initialize it by
   // assigning the vector views since we know that the data already fits in
   // memory since we passing it from R
-  TimeSeries_t *X = TimeSeries_alloc(nyrs, nobs);
+  TimeSeries_t *X = BLSP_TimeSeries_alloc(nyrs, nobs);
 
   gsl_vector_view data_view = gsl_vector_view_array(data_vec, nobs);
-  TimeSeries_set_data(X, &data_view.vector);
+  BLSP_TimeSeries_set_data(X, &data_view.vector);
 
   gsl_vector_view doy_view = gsl_vector_view_array(doy_vec, nobs);
-  TimeSeries_set_time(X, &doy_view.vector);
+  BLSP_TimeSeries_set_time(X, &doy_view.vector);
 
   gsl_vector_view idx_view = gsl_vector_view_array(year_idx_vec, nyrs + 1);
-  TimeSeries_set_tidx(X, &idx_view.vector);
+  BLSP_TimeSeries_set_tidx(X, &idx_view.vector);
 
 
   // This instead uses pointers to the underlying memory passed from R
   /// TimeSeries_t *Xi = TimeSeries_init(nyrs, nobs, &data_view.vector,
   /// &doy_view.vector, &yr_idx_view.vector);
 
-  blsp_workspace *w = blsp_workspace_sampler_alloc(nobs, nyrs, nburn, niter, 7);
+  blsp_workspace *w = BLSP_Workspace_alloc(nobs, nyrs, nburn, niter, 7);
 
   gsl_vector_view theta_mu_view = gsl_vector_view_array(theta_mu, 7);
   gsl_vector_view theta_sd_view = gsl_vector_view_array(theta_sd, 7);
 
-  int status = blsp_sampler(X, &theta_mu_view.vector, &theta_sd_view.vector, w);
+  int status = BLSP_sampler(X, &theta_mu_view.vector, &theta_sd_view.vector, w);
 
   // Copy over sampling mat
-  GSLMAT_TO_REALSXP(theta_matrix, blsp_workspace_samples(w));
+  GSLMAT_TO_REALSXP(theta_matrix, BLSP_Workspace_samples(w));
 
   // Clean up
-  TimeSeries_free(X);
-  blsp_free_workspace(w);
+  BLSP_TimeSeries_free(X);
+  BLSP_Workspace_free(w);
 
   // Pop R memory stack
   UNPROTECT(1); // -> theta_matrix
--- r/src/blsp_r.h
+++ r/src/blsp_r.h
@@ -2,6 +2,14 @@
 #include <R_ext/Rdynload.h>
 #include <Rinternals.h>
 
+#define GSL_TO_REALSXP(dst, src, size)                                         \
+  memcpy(REAL(dst), src, sizeof(double) * size);
+
+#define GSLVEC_TO_REALSXP(dst, src) GSL_TO_REALSXP(dst, src->data, src->size)
+
+#define GSLMAT_TO_REALSXP(dst, src)                                            \
+  GSL_TO_REALSXP(dst, src->data, src->size1 * src->size2)
+
 SEXP run_blsp(SEXP data_vector, SEXP doy_vector, SEXP year_idx_vector,
               SEXP init_theta_mu, SEXP init_theta_sd, SEXP iterations,
               SEXP burn);
