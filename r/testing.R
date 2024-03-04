2f2ca0a38c8dae95b4277bf4f3b78a2494eef258
ocsmit <osmith2@ncsu.edu>
2024-03-04 10:27:07 -0500

    [R] Update temp test script

--- r/testing.R
+++ r/testing.R
@@ -1,9 +1,8 @@
-data_path <- file.path("../test_data.Rdata")
+data_path <- file.path("test_data.Rdata")
 load(data_path)
 
 devtools::clean_dll()
-devtools::load_all(reset=T)
-
+devtools::load_all(reset = T)
 
 theta_mean <- c(
     -1.998392, 0.960355, 120.702350, 9.263498,
@@ -14,21 +13,5 @@ theta_sd <- c(
     1.55979462, 1.20727157, 0.19881890
 )
 
-
 tt <- blspR::blsp_test(y, t, theta_mean, theta_sd)
-
-mydt <- data.table::as.data.table(tt$samples)
-dt <- list()
-for (i in 0:19) {
-    year_dt = mydt[V2 == i]
-    theta3 <- quantile(year_dt[, V5], c(0.025, 0.5, 0.975))
-    theta5 <- quantile(year_dt[, V7], c(0.025, 0.5, 0.975))
-    dt[[i+1]] <- list(theta3[1], theta3[2], theta3[3],
-        theta5[1], theta5[2], theta5[3])
-}
-
-dtp <- data.table::rbindlist(dt)
-colnames(dtp) <- c("midgup_lower", "midgup", "midgup_upper",
-    "midgdown_lower", "midgdown", "midgdown_upper")
-
-dtp
+print(tt$pheno)
