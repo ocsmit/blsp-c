806e886ff562ef8b48c50abe0e179c4a7d5d4e86
ocsmit <osmith2@ncsu.edu>
2024-03-05 09:58:07 -0500

    [DOC] Add documentation for library headers

--- include/libblsp.h
+++ include/libblsp.h
@@ -6,6 +6,10 @@
 #include <gsl/gsl_matrix.h>
 
 typedef struct BLSP_TimeSeries_T BLSP_TimeSeries_T;
+/**
+ * @struct blsp_workspace
+ *   Holds all required data and structures for BLSP
+*/
 typedef struct BLSP_Workspace_T BLSP_Workspace_T;
 
 /**
@@ -27,17 +31,74 @@ extern BLSP_Workspace_T *BLSP_Workspace_alloc(const size_t nobs,
                                                     const size_t niter,
                                                     const size_t npar);
 
+
+/**
+ * @brief      Free workspace memory
+ *
+ * @details    Frees workspace and all objects needed by MCMC sampler
+ *
+ * @param      w
+ *
+ * @return     void
+ */
 extern void BLSP_Workspace_free(BLSP_Workspace_T *w);
 
+
+/**
+ * @brief      Get sample matrix
+ *
+ * @details    Returns a gsl_matrix* containing all MCMC samples indexed by year and iteration
+ *
+ * @param      w
+ *
+ * @return     gsl_matrix *
+ */
 extern gsl_matrix *BLSP_Workspace_samples(BLSP_Workspace_T *w);
 
+
+/**
+ * @brief      BLSP_TimeSeries_alloc
+ *
+ * @details    allocates memory for a timeseries struct
+ *
+ * @param      n_years
+ * @param      n_obs
+ *
+ * @return     *TimeSeries_t
+ */
 extern BLSP_TimeSeries_T *BLSP_TimeSeries_alloc(size_t n_years, size_t n_obs);
+
+/**
+ * @brief      Frees TimeSeries_t memory
+ *
+ * @details    Frees memory allocated by TimeSeries_alloc for a TimeSeries_t
+ * object
+ *
+ * @param      TSData
+ *
+ * @return     void
+ */
 extern void BLSP_TimeSeries_free(BLSP_TimeSeries_T *TSData);
+
 extern void BLSP_TimeSeries_set_data(BLSP_TimeSeries_T *TSData, gsl_vector *data);
 extern void BLSP_TimeSeries_set_time(BLSP_TimeSeries_T *TSData, gsl_vector *data);
 extern void BLSP_TimeSeries_set_tidx(BLSP_TimeSeries_T *TSData, gsl_vector *data);
 
 
+/**
+ * @brief      BLSP Algorithm
+ *
+ * @details    Runs MCMC sampler for BLSP
+ *
+ * @param      X          TimeSeries_t
+ * @param      theta_mu   mean parameter values to initialize the parameters for
+ * each year
+ * @param      theta_sd   standard deviation parameter values to initialize the
+ * parameters
+ * @param      w          workspace
+ *
+ * @return     int
+ */
 extern int BLSP_sampler(BLSP_TimeSeries_T *X, const gsl_vector *theta_mu,
                  const gsl_vector *theta_sd, BLSP_Workspace_T *w);
 
